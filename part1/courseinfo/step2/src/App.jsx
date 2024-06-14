0c372527918c570fa2bae24a7e2b83a1570a640b
Aarav <85437411+aaravkuxar@users.noreply.github.com>
2024-06-14 10:00:08 +0530

    added part1

---
+++ part1/courseinfo/step2/src/App.jsx
@@ -0,0 +1,54 @@
+const Part1 = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+
+  return(
+    <div>
+      <p>{part1} {exercises1}</p>
+    </div>
+  )
+}
+
+const Part2 = () => {
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+
+  return(
+    <div>
+      <p>{part2} {exercises2}</p>
+    </div>
+  )
+}
+
+const Part3 = () => {
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return(
+    <div>
+      <p>{part3} {exercises3}</p>
+    </div>
+  )
+}
+
+const Content = () => {
+
+  return(
+    <div>
+      <Part1 />
+      <Part2 />
+      <Part3 />
+    </div>
+  )
+}
+
+const App = () => {
+
+  return(
+    <div>
+      <Content />
+    </div>
+  )
+}
+
+export default App
