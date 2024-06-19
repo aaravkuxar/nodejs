79291e07d1953ebb6b2424476f7c2be7234319b0
Aarav Kumar <aarav.240604@gmail.com>
2024-06-19 15:06:30 +0530

    JavaScript Exercises

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,54 +1,50 @@
-const Part1 = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
 
-  return(
+const Content = ({ parts }) => {
+  return (
     <div>
-      <p>{part1} {exercises1}</p>
+      {parts.map((part, index) => (
+        <p key={index}>
+          {part.name} - {part.exercises}
+        </p>
+      ))}
     </div>
   )
 }
 
-const Part2 = () => {
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  return(
-    <div>
-      <p>{part2} {exercises2}</p>
-    </div>
-  )
-}
-
-const Part3 = () => {
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return(
-    <div>
-      <p>{part3} {exercises3}</p>
-    </div>
-  )
-}
-
-const Content = () => {
-
-  return(
-    <div>
-      <Part1 />
-      <Part2 />
-      <Part3 />
-    </div>
-  )
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Total number of exercises: {total}</p>
 }
 
 const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  return(
+  return (
     <div>
-      <Content />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-export default App
+export default App;
